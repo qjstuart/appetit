fd9a0fceee47a67f947fc0bb3af295f182f65313
qjstuart <quentinqjs@gmail.com>
2023-11-03 14:00:02 +0100

    Get cart size using context and display in header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import { useContext } from "react";
 
-import Logo from "../../assets/logo.jpg";
-// import HeaderCartButton from "./HeaderCartButton";
+import CartContext from "../../store/CartContext";
+import logo from "../../assets/logo.jpg";
 import Button from "../UI/Button";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const cartCtx = useContext(CartContext);
+
+  // We want to go through all items in the items array and accumulate the total quantity.
+  // The reducer method is perfect for this as it allows us to "reduce" an array down to one value.
+  const totalCartItems = cartCtx.items.reduce(
+    (accumulatedQuantity, currentItem) =>
+      accumulatedQuantity + currentItem.quantity,
+    0
+  );
+
   return (
     <header id={styles["main-header"]}>
       <div id={styles.title}>
-        <img src={Logo} alt="ReactFood logo. Table laid for one." />
+        <img src={logo} alt="ReactFood logo. Table laid for one." />
         <h1>Appétit</h1>
       </div>
-      <Button textButton>Cart (0)</Button>
+      <Button textButton>Cart ({totalCartItems})</Button>
     </header>
   );
 };
